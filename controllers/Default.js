834942ceb9d014df3c945216eca4442e1d347a92
Markus Sabadello <markus@danubetech.com>
2024-01-08 11:36:53 +0100

    Upgrade dependencies and add logging

--- controllers/Default.js
+++ controllers/Default.js
@@ -6,11 +6,15 @@ var Default = require('../service/DefaultService');
 module.exports.resolve = function resolve (req, res) {
     const identifier = req.params['identifier'];
     const accept = req.get('accept');
+    console.log('request: ' + identifier);
+    console.log('accept: ' + accept);
     Default.resolve(identifier, accept)
         .then(function (response) {
+            console.log('result: ' + JSON.stringify(response));
             utils.writeJson(res, response);
         })
         .catch(function (response) {
+            console.log('error: ' + JSON.stringify(response));
             utils.writeJson(res, response);
         });
 };
