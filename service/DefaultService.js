47be2c515fd66745abd9cc9d39f0614d983c3743
Markus Sabadello <markus@danubetech.com>
2025-03-27 14:04:17 +0100

    feat: Add support for jwk_jcs-pub

--- service/DefaultService.js
+++ service/DefaultService.js
@@ -1,6 +1,18 @@
 'use strict';
 
-const didKey = require('@transmute/did-key.js');
+const Resolver = require("did-resolver");
+const keyGetResolver = require("@cef-ebsi/key-did-resolver");
+
+const transmuteDidKey = require('@transmute/did-key.js');
+
+let registry = {};
+
+const resolver = keyGetResolver.getResolver();
+console.log("ebsi resolver is now: " + JSON.stringify(resolver));
+registry = { ...registry, ...resolver };
+
+const didResolver = new Resolver.Resolver(registry);
+console.log("resolver is now: " + JSON.stringify(didResolver));
 
 /**
  * Resolve a DID or other identifier.
@@ -11,35 +23,49 @@ const didKey = require('@transmute/did-key.js');
  **/
 exports.resolve = function(identifier,accept) {
   return new Promise(function(resolve, reject) {
-    didKey.resolve(identifier)
-    .then(function(didResolutionResult) {
-      if (didResolutionResult) {
-        const didDocument = didResolutionResult['didDocument'];
-        const verificationMethods = didDocument['verificationMethod'];
-        console.log("verificationMethods=" + typeof(verificationMethods));
-        for (const i in verificationMethods) {
-          console.log("i=" + i);
-          const verificationMethod = verificationMethods[i];
-          console.log("verificationMethod=" + JSON.stringify(verificationMethod));
-          const publicKeyJwk = verificationMethod ? verificationMethod['publicKeyJwk'] : undefined;
-          const crv = publicKeyJwk ? publicKeyJwk['crv'] : undefined;
-          if (crv === 'BLS12381_G1') {
-            publicKeyJwk['kty'] = 'OKP';
-            publicKeyJwk['crv'] = 'Bls12381G1';
+    if (identifier.startsWith('did:key:z2dm') || identifier.startsWith('did:key:zmYg') || identifier.startsWith('did:key:zhQR')) {
+      didResolver.resolve(identifier)
+        .then(function(didResolutionResult) {
+          if (didResolutionResult) {
+            resolve(didResolutionResult);
+          } else {
+            resolve(404);
           }
-          if (crv === 'BLS12381_G2') {
-            publicKeyJwk['kty'] = 'OKP';
-            publicKeyJwk['crv'] = 'Bls12381G2';
+        })
+        .catch(function (response) {
+          resolve({code:500, payload:''+response});
+        });
+    } else {
+      transmuteDidKey.resolve(identifier)
+      .then(function(didResolutionResult) {
+        if (didResolutionResult) {
+          const didDocument = didResolutionResult['didDocument'];
+          const verificationMethods = didDocument['verificationMethod'];
+          console.log("verificationMethods=" + typeof(verificationMethods));
+          for (const i in verificationMethods) {
+            console.log("i=" + i);
+            const verificationMethod = verificationMethods[i];
+            console.log("verificationMethod=" + JSON.stringify(verificationMethod));
+            const publicKeyJwk = verificationMethod ? verificationMethod['publicKeyJwk'] : undefined;
+            const crv = publicKeyJwk ? publicKeyJwk['crv'] : undefined;
+            if (crv === 'BLS12381_G1') {
+              publicKeyJwk['kty'] = 'OKP';
+              publicKeyJwk['crv'] = 'Bls12381G1';
+            }
+            if (crv === 'BLS12381_G2') {
+              publicKeyJwk['kty'] = 'OKP';
+              publicKeyJwk['crv'] = 'Bls12381G2';
+            }
           }
+          resolve(didDocument);
+        } else {
+          resolve(404);
         }
-        resolve(didDocument);
-      } else {
-        resolve(404);
-      }
-    })
-    .catch(function (error) {
-      console.error(error);
-      resolve({ code:500, payload:'' + error });
-    });
+      })
+      .catch(function (error) {
+        console.error(error);
+        resolve({ code:500, payload:'' + error });
+      });
+    }
   });
 };
